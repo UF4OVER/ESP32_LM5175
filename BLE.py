61bcb322dbbb9cf5b669bf684356cef6327f8fa8
UF4OVER <3397499417@qq.com>
2025-10-19 20:45:04 +0800

    Initial commit

---
+++ BLE.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# -------------------------------
+#  @Project : ESP32_LM5175
+#  @Time    : 2025 - 10-19 19:25
+#  @FileName: BLE.py
+#  @Software: PyCharm 2024.1.6 (Professional Edition)
+#  @System  : Windows 11 23H2
+#  @Author  : 33974
+#  @Contact : 
+#  @Python  : 
+# -------------------------------
